a645a65792d7b14ff7c445ce53185b16c09ce64d
hbaten <hbaten@LAPTOP-DNDPJNNA>
2026-08-28 09:46:16 -0600

    Entidad beneficiario - prueba de ramas y pr

---
+++ Domain/Entities/Beneficiario.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Beneficiario
+    {
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Cui { get; set; }
+    }
+}
